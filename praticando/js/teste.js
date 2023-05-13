f53d3c8389c156dd38236a82152613d74f5f2bad
lGabrielDev <thegabrielfreitasbf@yahoo.com.br>
2023-05-12 22:02:00 -0300

    iniciado resumo sobre querySelector

--- praticando/js/teste.js
+++ praticando/js/teste.js
@@ -1,26 +1,5 @@
-let pessoa1 = {
-    name: "adalberto",
-    age: 54,
-    
-    //getters and setters improvisado
-    getName: function getName(){
-        return this.name;
-    },
+const listaP = document.querySelectorAll("p");
 
-    setName: function setName(name){
-        this.name = name;
-    },
-
-    getAge: function getAge(){
-        return this.age;
-    },
-
-    setAge: function setAge(age){
-        this.age = age;
-    },
-
-    //toString()
-    toString: function toString(){
-        return `Name: ${this.name}\nAge: ${this.age}`;
-    }
+for(let i of listaP){
+    i.innerHTML = "arvore";
 }
